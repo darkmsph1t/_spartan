b7480b0129bde2a8320fa8155f24ef57a3218ba8
darkmsph1t <darkmsph1t@gmail.com>
2018-08-01 00:19:32 -0500

    cleaning up issues with resource sharing and cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 'use strict';
 var start = require("./start.js");
-var spartan = require("./spartan.js");
+//var spartan = require("./spartan.js");
 var wp = require('./write-policy.js');
 var path = require('path');
 var commander = require('commander');
@@ -29,9 +29,9 @@ async function begin (){
 
 }
 
-//console.log("Thanks for using _spartan! Here's how it works: \n * After answering a few questions, _spartan will generate a policy file (security.json).\n * Based upon the contents, _spartan generates the basic boilerplate code (security.js) which can be referenced in your application.\n * _spartan will also update the application's package.json file if additional dependencies are required.\n\n");
+console.log("Thanks for using _spartan! Here's how it works: \n * After answering a few questions, _spartan will generate a policy file (security.json).\n * Based upon the contents, _spartan generates the basic boilerplate code (security.js) which can be referenced in your application.\n * _spartan will also update the application's package.json file if additional dependencies are required.\n\n");
 
-spartan.banner("_spartan");
+//spartan.banner("_spartan");
 commander
   .version('0.0.1')
   .option('init, [--y][--yes]', 'Runs the configuration wizard, unless you also use the -y | -yes flag to just accept the policy defaults')
