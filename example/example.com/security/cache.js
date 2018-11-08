17316f5e12d0db4b7807747d2ec5b01a47bad83c
darkmsph1t <darkmsph1t@gmail.com>
2018-11-08 13:03:13 -0600

    rewrote caching.js in example. tests needed befo re inclusion into main code body

--- example/example.com/security/cache.js
+++ example/example.com/security/cache.js
@@ -5,52 +5,63 @@ const redis = require('redis')
 const REDIS_PORT = require('./secrets').fetchSecret('REDIS_PORT')
 const client = redis.createClient(REDIS_PORT)
 
-module.exports = function cacheSec () {
-  function setCacheHeaders () {
-    let c = secJson.securityHeaders.caching.cacheControl
-    let cc = ''
-    for (let h = 0; h < c.length; h++) {
-      cc = cc + c[h] + ', '
-    }
-    let v = secJson.securityHeaders.caching.vary
-    let vv = ''
-    for (let i = 0; i < v.length; i++) {
-      vv = vv + v[i] + ', '
-    }
-    let cacheHeaders = {
-      'Cache-Control': cc,
-      pragma: secJson.securityHeaders.caching.pragma,
-      vary: vv
-    }
-    return cacheHeaders
+function setCacheHeaders () {
+  let c = secJson.securityHeaders.caching.cacheControl
+  let cc = ''
+  for (let h = 0; h < c.length; h++) {
+    cc = cc + c[h] + ', '
   }
-  const setCache = async function (route, fetchedThing) {
-    if (secJson.caching.routeOverload === false && route !== undefined) {
-      let error = new Error(`Sorry! Route overload has been disabled for ${route}`)
-      return error
-    } else {
-      try {
-        await client.setex(route, secJson.caching.ttl, fetchedThing)
-      } catch (err) {
-        return err
-      }
-    }
+  let v = secJson.securityHeaders.caching.vary
+  let vv = ''
+  for (let i = 0; i < v.length; i++) {
+    vv = vv + v[i] + ', '
   }
-  const getCache = async function (route, fetchedThing) {
-    client.get(route, function (err, data) {
-      if (err) throw err
-      if (data !== null) {
-        return data
-      } else {
-        return undefined
-      }
-    })
+  let cacheHeaders = {
+    'Cache-Control': cc,
+    pragma: secJson.securityHeaders.caching.pragma,
+    vary: vv
   }
+  return cacheHeaders
+}
+
+module.exports = () => {
+  // check to see if cache protection is enabled
   if (secJson.securityHeaders.caching.enabled === false) {
-    let error = new Error('Caching is not enabled for this application')
+    let error = new Error('cache/cache-policy-disabled')
+    error.message = 'Caching is not enabled for this application'
     return error
   } else {
-    let cacheHeaders = setCacheHeaders()
-    return cacheHeaders
+    return {
+      cacheHeaders: setCacheHeaders(),
+      setCache: (route, fetchedThing, callback) => {
+        if (secJson.caching.routeOverload === false && route !== undefined) {
+          let error = new Error('cache/route-overload-disabled')
+          error.message = `Sorry! Route overload has been disabled for ${route}`
+          callback(error, null)
+        } else {
+          try {
+            client.setex(route, secJson.caching.ttl, fetchedThing)
+            let data = 'success'
+            callback(null, data)
+          } catch (err) {
+            callback(err, null)
+          }
+        }
+      },
+      getCache: async (route, fetchedThing, callback) => {
+        client.get(route, function (err, data) {
+          if (err) {
+            err.code = '(cache/could-not-fetch-data)'
+            err.message = `Could not fetch data for ${route}`
+            callback(err, null)
+          }
+          if (data !== null) {
+            callback(null, data)
+          } else {
+            return undefined
+          }
+        })
+      }
+    }
   }
 }
